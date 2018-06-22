b54142e55249438b034b389d6bd021615693efb2
Kendall Haechten <bumbera72@gmail.com>
2018-06-21 20:31:03 -0500

    object

---
+++ object.rb
@@ -0,0 +1,28 @@
+class Horse
+
+    def set_name=(horse_name)
+      @name = horse_name
+    end
+
+    def get_name
+      return @get_name
+    end
+
+    def set_owner=(owner_name)
+      @owner_name = owner_name
+    end
+
+    def get_owner
+      return @owner_name
+    end
+
+    def squeal
+      return "squeeeee"
+    end
+
+end
+
+my_horse = Horse.new
+my_horse.set_name= "Hoofey"
+horse_name = my_horse.get_name
+puts "#{horse_name} says #{my_horse.squeal}"
